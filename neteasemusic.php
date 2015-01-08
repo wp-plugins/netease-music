3849cc838856c7a76830ed1402b71e9ef3f2d181
fatesinger <fatesinger@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 07:31:03 +0000

    1.5.0
    git-svn-id: https://plugins.svn.wordpress.org/netease-music/trunk@1062846 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ neteasemusic.php
@@ -0,0 +1,21 @@
+<?php 
+/*
+Plugin Name: 网易云音乐
+Plugin URI: http://fatesinger.com/74369
+Description: 网易云音乐
+Version: 1.5.0
+Author: Bigfa
+Author URI: http://fatesinger.com
+*/	
+
+define('NM_VERSION', '1.5.0');
+define('NM_URL', plugins_url('', __FILE__));
+define('NM_PATH', dirname( __FILE__ ));
+define('NM_ADMIN_URL', admin_url());
+
+require NM_PATH . '/nmjson.php';
+
+require NM_PATH . '/functions.php';
+
+
+
